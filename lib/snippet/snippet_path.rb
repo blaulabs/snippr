2a47f18b813ad06e7b11880ced99477f822fdeb7
rubiii <me@rubiii.com>
2010-04-03 18:03:51 +0200

    the library is now compatible with ruby 1.8.7, 1.9.1 and jruby 1.4

--- lib/snippet/snippet_path.rb
+++ lib/snippet/snippet_path.rb
@@ -9,27 +9,38 @@ module SnippetPath
 
   # Returns the snippet path from JVM properties.
   def snippet_path
-    path = JavaLang::System.get_property SnippetPathProperty
-    invalid_snippet_path(path) unless path && File.directory?(path)
-    path
+    path = JavaLang::System.get_property(SnippetPathProperty) rescue @snippet_path
+    validate_snippet_path path
   end
 
   # Sets the snippet path as a JVM property.
   def snippet_path=(path)
-    invalid_snippet_path(path) unless File.directory? path
-    JavaLang::System.set_property SnippetPathProperty, path
+    validate_snippet_path path
+    JavaLang::System.set_property(SnippetPathProperty, path) if jruby?
+    @snippet_path = path
   end
 
 private
 
-  # Raises an ArgumentError complaining about an invalid snippet path.
-  def invalid_snippet_path(path = nil)
-    raise ArgumentError, "Invalid snippet_path: '#{path}'"
+  # Validates a given snippet +path+. Raises an ArgumentError in case of an
+  # invalid snippet path or just returns the original path.
+  def validate_snippet_path(path = nil)
+    raise ArgumentError, "Missing snippet path!" unless path
+    raise ArgumentError, "Invalid snippet_path: #{path}" unless File.directory? path
+    path
   end
 
-  # Contains the java.lang package.
-  module JavaLang
-    include_package "java.lang"
+  # Returns whether the current Ruby platform is JRuby.
+  def jruby?
+    RUBY_PLATFORM =~ /java/
+  end
+
+  module_function :jruby?
+
+  if jruby?
+    module JavaLang
+      include_package "java.lang"
+    end
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 require "spec"
+require "date"
+
 require "snippet"
