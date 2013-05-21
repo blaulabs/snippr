72d96e36f4beabf9689cb592c67b0206fbd82d15
Frank Schumacher <frank.schumacher.privat@gmail.com>
2013-05-21 09:31:14 +0200

    remove shoulds

--- spec/snippr/snippr_spec.rb
+++ spec/snippr/snippr_spec.rb
@@ -1,38 +1,39 @@
+# -*- encoding : utf-8 -*-
 require "spec_helper"
 
 describe Snippr do
 
-  it "should delegate path to Snippr::Path.path" do
+  it "delegates path to Snippr::Path.path" do
     Snippr::Path.should respond_to(:path)
     Snippr::Path.expects(:path).returns('path')
     subject.path.should == 'path'
   end
 
-  it "should delegate path= to Snippr::Path.path=" do
+  it "delegates path= to Snippr::Path.path=" do
     Snippr::Path.should respond_to(:path=)
     Snippr::Path.expects(:path=).with('path')
     subject.path = 'path'
   end
 
-  it "should delegate i18n? to Snippr::I18n.enabled?" do
+  it "delegates i18n? to Snippr::I18n.enabled?" do
     Snippr::I18n.should respond_to(:enabled?)
     Snippr::I18n.expects(:enabled?).returns(true)
     subject.i18n?.should == true
   end
 
-  it "should delegate i18n= to Snippr::I18n.enabled=" do
+  it "delegates i18n= to Snippr::I18n.enabled=" do
     Snippr::I18n.should respond_to(:enabled=)
     Snippr::I18n.expects(:enabled=).with(true)
     subject.i18n = true
   end
 
-  it "should delegate adjust_urls_except? to Snippr::Links.adjust_urls_except" do
+  it "delegates adjust_urls_except? to Snippr::Links.adjust_urls_except" do
     Snippr::Links.should respond_to(:adjust_urls_except)
     Snippr::Links.expects(:adjust_urls_except).returns([1])
     subject.adjust_urls_except.should == [1]
   end
 
-  it "should delegate adjust_urls_except= to Snippr::Links.adjust_urls_except=" do
+  it "delegates adjust_urls_except= to Snippr::Links.adjust_urls_except=" do
     Snippr::Links.should respond_to(:adjust_urls_except=)
     Snippr::Links.expects(:adjust_urls_except=).with([2])
     subject.adjust_urls_except = [2]
@@ -71,12 +72,12 @@ describe Snippr do
 
   end
 
-  it "should delegate load to Snippr::Snip.new" do
+  it "delegates load to Snippr::Snip.new" do
     Snippr::Snip.expects(:new).with(:a, :b).returns('snip')
     subject.load(:a, :b).should == 'snip'
   end
 
-  it "should delegate list to Snippr::Path.list" do
+  it "delegates list to Snippr::Path.list" do
     Snippr::Path.expects(:list).with(:c, :d).returns([:snip])
     subject.list(:c, :d).should == [:snip]
   end
