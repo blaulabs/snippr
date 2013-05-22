a930660cadb23a0b09de1c269d6a09b6bca934c7
Frank Schumacher <frank.schumacher.privat@gmail.com>
2013-05-22 13:28:18 +0200

    use clock in valid_* segmentfilters

--- lib/snippr/segment_filter/valid_until.rb
+++ lib/snippr/segment_filter/valid_until.rb
@@ -3,7 +3,7 @@ module Snippr
   module SegmentFilter
     class ValidUntil < Base
       def active?
-        DateTime.now.to_s <= DateTime.strptime(@filter_value, "%Y-%m-%d %H:%M").to_s
+        Snippr::Clock.now.to_s <= DateTime.strptime(@filter_value, "%Y-%m-%d %H:%M").to_s
       rescue ArgumentError
         false
       end
