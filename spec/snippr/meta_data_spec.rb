39cb8c9f01cba7cbc43ecc7d7e6bf4d74d3e054f
Andre Jährling <andre.jaehrling@blau.de>
2014-05-20 16:49:13 +0200

    add tests for metadata.extract (fancy stuff)

---
+++ spec/snippr/meta_data_spec.rb
@@ -0,0 +1,40 @@
+# -*- encoding : utf-8 -*-
+require "spec_helper"
+
+describe Snippr::MetaData do
+  include Snippr::ViewHelper
+
+  TEST_CONTENT = 'Hier ist jede Menge Content.'
+  TEST_CONTENT_WITH_METABLOCK = "---\nyml_key: yml_value\n---\n#{TEST_CONTENT}"
+
+  describe '.extract' do
+    it 'returns an array with 2 elements [contentstring, metahash]' do
+      result = Snippr::MetaData.extract([:content], TEST_CONTENT)
+      expect(result).to be_a Array
+      expect(result).to have(2).items
+      expect(result.first).to be_a String
+      expect(result.second).to be_a Hash
+    end
+
+    it 'returns raw content for non existing metablock' do
+      result = Snippr::MetaData.extract([:content], TEST_CONTENT)
+      expect(result.first).to eq TEST_CONTENT
+    end
+
+    it 'returns empty hash for non existing metablock' do
+      result = Snippr::MetaData.extract([:content], TEST_CONTENT)
+      expect(result.second).to eq({})
+    end
+
+    it 'returns a hash from given metablock' do
+      result = Snippr::MetaData.extract([:content], TEST_CONTENT_WITH_METABLOCK)
+      expect(result.second).to eq({'yml_key' => 'yml_value'})
+    end
+
+    it 'returns content as content without metablock' do
+      result = Snippr::MetaData.extract([:content], TEST_CONTENT_WITH_METABLOCK)
+      expect(result.first).to eq TEST_CONTENT
+    end
+  end
+
+end
